1a6070e73856e161841a46f01abbc7f9f1802861
Rui Holdorf <ruih@pbtech.net.br>
2021-01-06 19:06:15 -0300

    initial commit

---
+++ CaffeineApplicationContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Caffeine
+{
+    public class CaffeineApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+        private readonly CursorObserver _cursorObserver = new CursorObserver();
+
+        public CaffeineApplicationContext()
+        {
+            var icon = GetIcon("16h.png");
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = icon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Sair", Exit)
+            }),
+                Visible = true,
+                Text = "Caffeine"
+            };
+
+            _cursorObserver.Start();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            _cursorObserver.Stop();
+            _trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private Icon GetIcon(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourcePath = assembly.GetManifestResourceNames().Single(str => str.EndsWith(resourceName));
+            using (var stream = assembly.GetManifestResourceStream(resourcePath))
+            {
+                using (Bitmap bitmap = (Bitmap)Image.FromStream(stream))
+                {
+                    return Icon.FromHandle(bitmap.GetHicon());
+                }
+            }
+        }
+    }
+}
---
+++ CursorObserver.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Caffeine
+{
+    class CursorObserver
+    {
+        private Thread _thread;
+        private bool _keepRunning;
+        private Point _lastPosition = new Point(0, 0);
+        private int _samePositionCounter = 0;
+        private readonly int _limitSamePositionCounter = 60;
+
+        public void Start()
+        {
+            _keepRunning = true;
+            _thread = new Thread(() => Observe()) { IsBackground = true };
+            _thread.Start();
+        }
+
+        private void Observe()
+        {
+            while (_keepRunning)
+            {
+                Thread.Sleep(1000);
+                if (IsInSamePlaceForTooLong())
+                    MoveAround();
+            }
+        }
+
+        private bool IsInSamePlaceForTooLong()
+        {
+            var currerntPosition = Cursor.Position;
+            if (_lastPosition.X == currerntPosition.X && _lastPosition.Y == currerntPosition.Y)
+                _samePositionCounter++;
+
+            _lastPosition = currerntPosition;
+            return _samePositionCounter > _limitSamePositionCounter;
+        }
+
+        private void MoveAround()
+        {
+            var position = Cursor.Position;
+            for (int i = 0; i <= 5; i++)
+            {
+                position.Y++;
+                position.X++;
+                Cursor.Position = position;
+                Thread.Sleep(10);
+            }
+            for (int i = 0; i <= 5; i++)
+            {
+                position.Y--;
+                position.X--;
+                Cursor.Position = position;
+                Thread.Sleep(10);
+            }
+            for (int i = 0; i <= 5; i++)
+            {
+                position.Y--;
+                position.X--;
+                Cursor.Position = position;
+                Thread.Sleep(10);
+            }
+            for (int i = 0; i <= 5; i++)
+            {
+                position.Y++;
+                position.X++;
+                Cursor.Position = position;
+                Thread.Sleep(10);
+            }
+
+            Cursor.Position = _lastPosition; // volta onde estava
+            _samePositionCounter = 0;
+        }
+
+        public void Stop()
+        {
+            _keepRunning = false;
+            _thread.Abort();
+        }
+    }
+}
